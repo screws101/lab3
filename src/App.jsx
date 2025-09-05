ccdf8e7b5aca4a3d786858496a8d4a4b95cc31d0
screws101 <142944513+screws101@users.noreply.github.com>
2025-09-05 10:55:29 -0400

    initial commit lab4

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,49 @@ import Card1 from './components/Card1';
 import Card2 from './components/Card2';
 import About from './components/About';
 import Header from './components/Header';
+import Card from './components/Card';
+import Wrapper from './components/Wrapper';
+import imgMan from './assets/headshot-man.jpg';
+import imgWoman from './assets/headshot-woman.jpg';
+
 
 function App() {
-
+  const profiles = [
+    {name: "John Doe", title: "Software Engineer", email:"john.@email.org", img: imgMan},
+    {name: "Jane Doe", title: "UX Designer", email:"jane.@email.org", img: imgWoman}
+  ]
   return (
     <>
-      <Header/>
-      <h1>Profile App</h1>
-      <div className="section">
-        <div className="container">
-          <About/>
+      <header>
+        <Header/>
+      </header>
+      <main>
+        <div className="section">
+          <div className="container">
+            <div className="page-title">
+            <h1>Profile App</h1>
+          </div>
+          </div>
         </div>
-      </div>
-      <div className="section">
-        <div className="card_container">
-          <Card1/>
-          <Card2/>
+
+        <div className="section">
+          <div className="about-container">
+            <About/>
+          </div>
         </div>
-      </div>
+
+        <div className="section" id="profiles">
+          <div className="container">
+            <div className="flex-container">
+              {
+                profiles.map((profile,index) => (
+                  <Card key={index} name={profile.name} title={profile.title} email={profile.email} img={profile.img} />
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      </main>
 
     </>
   )
